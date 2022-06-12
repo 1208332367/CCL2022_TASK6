04173d1e4796449d2ad16c08d04d5aac891fafb6
chengchao23 <55905375+chengchao23@users.noreply.github.com>
2022-06-12 12:34:29 +0800

    Add files via upload

--- 2022/model.py
+++ 2022/model.py
@@ -17,9 +17,10 @@ class Model(nn.Module):
         self.gru = nn.GRU(self.config.hidden_size, hidden_states, num_layers, bidirectional, dropout=dropout)
         # self.cross_attention = torch.nn.MultiheadAttention(embed_dim=attention_states, num_heads=num_heads,
         #                                                    dropout=dropout)
-        self.lin = nn.Linear(self.config.hidden_size, hidden_states)
-        self.event_start_lin = nn.Linear(1, output_states)
-        self.event_end_lin = nn.Linear(1, output_states)
+        self.lin1 = nn.Linear(self.config.hidden_size, hidden_states)
+        self.lin2 = nn.Linear(self.config.hidden_size, hidden_states)
+        # self.event_start_lin = nn.Linear(1, output_states)
+        # self.event_end_lin = nn.Linear(1, output_states)
 
     def forward(self, descriptor, sentences):
         #  [(batch_size), (batch_size), ... , len(sentences)-1 ] ->  [batch_size * len(sentences)]
@@ -58,17 +59,19 @@ class Model(nn.Module):
                                     descriptor_attention_mask).pooler_output
         descriptor_encode = descriptor_encode.unsqueeze(1)
         # [batch_size, 1, 768]
-        descriptor_encode = self.lin(descriptor_encode)
+        descriptor_encode_start = self.lin1(descriptor_encode)
+        descriptor_encode_end = self.lin2(descriptor_encode)
         # [batch_size, 1, hidden_states]
-        sentences_encode = torch.bmm(sentences_encode, descriptor_encode.transpose(1, 2))
+        sentences_encode_start = torch.bmm(sentences_encode, descriptor_encode_start.transpose(1, 2))
+        sentences_encode_end = torch.bmm(sentences_encode, descriptor_encode_end.transpose(1, 2))
         # [batch_size, sentences_seqs(38), 1]
 
         # cross-attention
         # attn_output, attn_output_weights = self.cross_attention(gru_output, descriptor_encode, descriptor_encode)
         # [batch_size, sentences_seqs(38), attention_states]
 
-        start = self.event_start_lin(sentences_encode).sigmoid()
+        start = sentences_encode_start.sigmoid()
         # [batch_size, sentences_seqs(38), output_states]
-        end = self.event_end_lin(sentences_encode).sigmoid()
+        end = sentences_encode_end.sigmoid()
         # [batch_size, sentences_seqs(38), output_states]
         return start, end
