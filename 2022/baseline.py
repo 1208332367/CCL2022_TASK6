e579c43e0429dae1e54279fff99b9f6858c57ae5
chengchao23 <55905375+chengchao23@users.noreply.github.com>
2022-06-15 18:03:37 +0800

    Add files via upload

--- 2022/baseline.py
+++ 2022/baseline.py
@@ -4,13 +4,13 @@ from dataset import ECOMDatasets
 from torch.utils.data import DataLoader
 from model import Model
 import torch
-from MaskedBECLoss import MaskBECLoss
+from MaskedBECLoss import MaskBECLoss, toOnehot
 
 datasets = ECOMDatasets("./ECOM2022/data/train.doc.json", "./ECOM2022/data/train.ann.json")
 dev_datasets = ECOMDatasets("./ECOM2022/data/dev.doc.json", "./ECOM2022/data/dev.ann.json")
-dataloader = DataLoader(datasets, batch_size=16, shuffle=True)
-dev_dataloader = DataLoader(dev_datasets, batch_size=16, shuffle=False)
-model = Model(hidden_states=256, num_layers=2, dropout=0.3, sentence_max_length=512, descriptor_max_length=20)
+dataloader = DataLoader(datasets, batch_size=8, shuffle=True)
+dev_dataloader = DataLoader(dev_datasets, batch_size=8, shuffle=False)
+model = Model(hidden_states=256, num_layers=2, dropout=0.2, sentence_max_length=512, descriptor_max_length=20)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # for name, param in model.named_parameters():
@@ -37,7 +37,8 @@ model.to(device)
 # start和end的shape: torch.Size([2, 38, 2])   batch_size, seq_lens, target_label(0,1)
 
 num_epochs = 10
-lr = 0.2
+lr = 0.02
+threshold = 0.8
 # optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=lr)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -58,14 +59,18 @@ for epoch in range(num_epochs):
         optimizer.step()
     total = 0
     acc = 0
-    for i, (descriptors, contents, start_anns, end_anns, valid_lens, _) in enumerate(dataloader):
+    for i, (descriptors, contents, start_anns, end_anns, valid_lens, _) in enumerate(dev_dataloader):
         start_anns = start_anns.to(device).type(torch.float32)
         end_anns = end_anns.to(device).type(torch.float32)
         with torch.no_grad():
             start, end = model(descriptors, contents, device)
-        start_dev = [i[:v].equal(j[:v]) for i, j, v in zip(start.squeeze(-1), start_anns, valid_lens)]
-        end_dev = [i[:v].equal(j[:v]) for i, j, v in zip(end.squeeze(-1), end_anns, valid_lens)]
+        start = toOnehot(start.squeeze(-1), threshold)
+        end = toOnehot(end.squeeze(-1), threshold)
+        start_dev = [i[:v].equal(j[:v]) for i, j, v in zip(start, start_anns, valid_lens)]
+        end_dev = [i[:v].equal(j[:v]) for i, j, v in zip(end, end_anns, valid_lens)]
         static = [i & j for i, j in zip(start_dev, end_dev)]
         total += len(static)
         acc += torch.tensor(numpy.array(static)).sum()
     print('acc:', acc / total)
+
+
